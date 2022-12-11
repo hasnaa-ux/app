f87ffdca3420b481700aa7af9b1e6d3532630f13
hasnaa <hasnaa3193378@gmail.com>
2022-12-11 17:52:29 +0200

    comment1

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
+ 
   <v-row justify="center" align="center">
+    <h2>home</h2>
     <v-col cols="12" sm="8" md="6">
       <v-card class="logo py-4 d-flex justify-center">
         <NuxtLogo />
@@ -7,7 +9,7 @@
       </v-card>
       <v-card>
         <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
+          Welcome to the Vuetify
         </v-card-title>
         <v-card-text>
           <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
